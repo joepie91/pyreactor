70c701a00fed29aa07cd8276de8c12ba1464814f
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-01-23 11:50:24 +0100

    Add profiling

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pyreactor, time
 from testclient import TestClient
+import cProfile as profile
 
-s = pyreactor.Server("127.0.0.1", 4006, TestClient)
-c = TestClient(host="127.0.0.1", port=4006)
+def main():
+	s = pyreactor.Server("127.0.0.1", 4006, TestClient)
+	c = TestClient(host="127.0.0.1", port=4006)
 
-c.send({"test": "just sending some test data...", "number": 41, "file": open("testdata.dat", "rb")})
+	c.send({"test": "just sending some test data...", "number": 41, "file": open("testdata.dat", "rb")})
 
-reactor = pyreactor.Reactor()
-reactor.add(s)
-reactor.add(c)
-reactor.loop()
+	reactor = pyreactor.Reactor()
+	reactor.add(s)
+	reactor.add(c)
+	reactor.loop()
+
+profile.run("main()")
